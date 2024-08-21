d1361c478c6f11554bfe7a92847cafb32b699da5
TauaRibeiro <166765664+TauaRibeiro@users.noreply.github.com>
2024-08-20 21:28:43 -0300

    Update controle_escolar.sql

--- controle_escolar.sql
+++ controle_escolar.sql
@@ -1,24 +1,39 @@
+CREATE DATABASE controle_escolar;
+
 USE controle_escolar;
 
 CREATE TABLE `aluno` (
   `matricula` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
   `data_nascimento` date NOT NULL,
-  `tipo_logradouro` varchar(10) NOT NULL,
-  `endereco` varchar(150) NOT NULL,
+  `celular` varchar(14) NOT NULL,
   PRIMARY KEY (`matricula`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `controle_escolar`.`aluno`
-(`matricula`,
-`nome`,
-`data_nascimento`,
-`tipo_logradouro`,
-`endereco`)
-VALUES
-(<{matricula: }>,
-<{nome: }>,
-<{data_nascimento: }>,
-<{tipo_logradouro: }>,
-<{endereco: }>);
+CREATE TABLE `coordenador` (
+  `cpf_coordenador` varchar(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `data_nascimento` date NOT NULL,
+  `celular` varchar(14) NOT NULL,
+  PRIMARY KEY (`cpf_coordenador`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE `curso` (
+  `id_curso` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_curso` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_curso`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `disciplinas` (
+  `id_disciplina` int(11) NOT NULL,
+  `nome_disciplina` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_disciplina`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `professor` (
+  `matricula` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `data_nascimento` int(11) NOT NULL,
+  `celular` varchar(14) NOT NULL,
+  PRIMARY KEY (`matricula`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
